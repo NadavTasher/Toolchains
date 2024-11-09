920a9c4ffc8aacad2fc769f0860c6f43993f9e1c
Nadav Tasher <tashernadav@gmail.com>
2024-11-09 14:59:15 +0200

    WIP on makefiles

--- example/toolchains.mk
+++ example/toolchains.mk
@@ -1,14 +1,3 @@
-# Target compilation architecture
-ARCH ?= x86_64
-LIBC ?= MUSL
-
-# Toolchain prefixes
-MUSL_PREFIX := $(ARCH)-buildroot-linux-musl-
-UCLIBC_PREFIX := $(ARCH)-buildroot-linux-uclibc-
-
-# Toolchain prefix
-PREFIX ?= $(LIBC)_PREFIX
-
 # Toolchains container image
 IMAGE_TAG ?= latest
 IMAGE_NAME ?= nadavtasher/toolchains
@@ -18,11 +7,6 @@ BUILDER_HOSTNAME := builder
 
 # Command to execute in builder
 BUILDER_COMMAND ?= bash
-
-# Check whether binfmt should be used
-ifneq ($(shell uname -m),$(ARCH))
-PREREQUISITES := binfmt
-endif
 
 # If the hostname does not match the builder hostname, abort
 ifneq ($(shell hostname),$(BUILDER_HOSTNAME))
@@ -38,6 +22,6 @@ binfmt:
 	@docker run --rm --privileged $(IMAGE_NAME):$(IMAGE_TAG) qemu-binfmt-conf.sh
 
 .PHONY: container
-container: $(PREREQUISITES)
+container:
 	@docker run --rm -it -v $$PWD:/build:ro -v $$PWD/bin:/build/bin:rw -w /build -h builder --tmpfs /tmp $(IMAGE_NAME):$(IMAGE_TAG) $(BUILDER_COMMAND)
 endif
