0e6883ecc631e9cc821c72fd83303efa9d5287e8
Nadav Tasher <tashernadav@gmail.com>
2024-11-09 14:39:05 +0200

    More work on makefile

--- example/toolchains.mk
+++ example/toolchains.mk
@@ -1,3 +1,15 @@
+# Target compilation architecture
+ARCH ?= x86_64
+LIBC ?= MUSL
+
+# Toolchain prefixes
+MUSL_PREFIX := $(ARCH)-buildroot-linux-musl-
+UCLIBC_PREFIX := $(ARCH)-buildroot-linux-uclibc-
+
+# Toolchain prefix
+PREFIX ?= $(LIBC)_PREFIX
+
+# Toolchains container image
 IMAGE_TAG ?= latest
 IMAGE_NAME ?= nadavtasher/toolchains
 
@@ -7,7 +19,12 @@ BUILDER_HOSTNAME := builder
 # Command to execute in builder
 BUILDER_COMMAND ?= bash
 
-# If the repository has uncommited changes, tag the image as dirty
+# Check whether binfmt should be used
+ifneq ($(shell uname -m),$(ARCH))
+PREREQUISITES := binfmt
+endif
+
+# If the hostname does not match the builder hostname, abort
 ifneq ($(shell hostname),$(BUILDER_HOSTNAME))
 
 .PHONY: all
@@ -16,7 +33,11 @@ all:
 	@echo "Please run 'make container'."
 	@false
 
+.PHONY: binfmt
+binfmt:
+	@docker run --rm --privileged $(IMAGE_NAME):$(IMAGE_TAG) qemu-binfmt-conf.sh
+
 .PHONY: container
-container:
+container: $(PREREQUISITES)
 	@docker run --rm -it -v $$PWD:/build:ro -v $$PWD/bin:/build/bin:rw -w /build -h builder --tmpfs /tmp $(IMAGE_NAME):$(IMAGE_TAG) $(BUILDER_COMMAND)
 endif
